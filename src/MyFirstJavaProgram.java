f5ce88cc3c369764eebd3459a362718db1ffca14
mfarukaydin06 <mfarukaydin06@icloud.com>
2026-08-03 16:49:24 +0300

    Gün 1: İlk Java projesi yapıldı.

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram {
+
+    /* Bu benim ilk java programım.
+        Çıktı olarak 'Hello World.' ve 'Gün 1 - Java 101' verecek.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        System.out.println("Gün 1 - Java 101");
+    }
+}
